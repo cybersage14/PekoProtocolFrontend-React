9ea187be2698b40d01b9ed81be1d66abb924b14e
DarenGit28 <daren.shamoun28@gmail.com>
2023-07-19 01:39:20 +0900

    liquidate

--- src/pages/Liquidate/LiquidateDialog.tsx
+++ src/pages/Liquidate/LiquidateDialog.tsx
@@ -61,7 +61,7 @@ export default function LiquidateDialog({ visible, setVisible, closeLiquidateDia
     address: USDC_CONTRACT_ADDRESS,
     abi: USDC_CONTRACT_ABI,
     functionName: 'approve',
-    args: [POOL_CONTRACT_ADDRESS, parseUnits(`${usdcAmountToPay}`, USDC_DECIMAL)],
+    args: [POOL_CONTRACT_ADDRESS, parseUnits(`${usdcAmountToPay + MINOR_PLUS_FOR_APPROVE}`, USDC_DECIMAL)],
   })
   const { write: approve, data: approveData } = useContractWrite(approveConfig);
   const { isLoading: approveIsLoading, isError: approveIsError } = useWaitForTransaction({
